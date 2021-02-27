3ad60128039f0d4a969d3aed25121031fb2de9de
jayeebee <pnuthexsa311@gmail.com>
2021-02-27 02:17:55 -0500

    add looping fight

--- assets/js/game.js
+++ assets/js/game.js
@@ -19,10 +19,12 @@ for(var i = 0; i < enemyNames.length; i++) {
 var enemyHealth = 50;
 var enemyAttack = 12;
 
+var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+
 var fight = function(enemyName) {
     // Alert players that they are starting the round
-    window.alert("Welcome to Robot Gladiators!");
-    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+    while(enemyHealth > 0)
+
   
 // if player choses to fight, then fight
 if (promptFight === "fight" || promptFight === "FIGHT") {
@@ -71,5 +73,7 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
 };
 
 for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
+    var pickedEnemyName = enemyNames [i];
+    enemyHealth = 50;
+    fight( pickedEnemyName)
   }
